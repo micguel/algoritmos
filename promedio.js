980de8a212a1bbad1a42452fd3a852a0b6e5b6cd
MIGUEL ANGEL ANGEL <90216281+micguel@users.noreply.github.com>
2021-10-15 12:49:59 +0000

    empezar el .js

---
+++ promedio.js
@@ -0,0 +1,17 @@
+function cacularPromedio() {
+  //1.Inicio
+  //Definir c1, c2, c3, c4, c5, promedio
+  var c1;
+  var c2;
+  var c3;
+  var c4;
+  var c5;
+  var promedio;
+  //Hacer c1 = 10, c2 = 7.5, c3 = 9, c4 = 10, c5 = 10
+  c1 = 10
+  c2 = 7.5
+  c3 = 9
+  c4 = 10
+  c5 = 10
+  
+}
